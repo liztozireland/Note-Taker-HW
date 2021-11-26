c9e8baafc2cfa4aa3523ac52bc1742eb0ce08b33
Liz Ireland <liz.t.ireland@gmail.com>
2021-11-25 21:18:22 -0800

    updating correct file structure

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const path = require('path');
+const db = require('./db/db.json');
+const fs = require('fs');
+const {get} = require('http');
 
 const app = express();
 const PORT = 8081;
 
-app.get('/', (req,res) =>{
-    res.send('Hello World')
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+  })
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'))
+  })
+  app.get('/api/notes', (req, res) => {
+    res.json(db)
+  });
 
 
 
